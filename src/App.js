65eb325040acc648b4a03758cc47ce23a00a1fe3
anmallie <96325117+anmallie@users.noreply.github.com>
2023-04-05 15:29:37 -0700

    Adding logic to create an oscillating/constricting hemisphere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import { Canvas } from '@react-three/fiber'
+import { useState, useRef } from 'react'
 
-function Sphere() {
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function ConstrictingHemisphere() {
+  const mesh = useRef()
+  const [thetaLength, setThetaLength] = useState(Math.PI/4)
+  const [radius, setRadius] = useState(2)
+  
+  const updateThetaLength = (time) => {
+    // oscillate thetaLength according to sin(x)/2.6 + 3pi/8
+    const newThetaLength = Math.sin(time)/2.6 + 3*Math.PI/8;
+    setThetaLength(newThetaLength);
+  }
+  const updateRadius = (time) => {
+    // oscillate radius between some numbers
+    const offset = Math.PI // 
+    const newRadius = Math.sin(time + offset)/2 + 1.5;
+    setRadius(newRadius);
+  }
+  useFrame(({clock}) => {
+      const timescale = 1;
+      const time = clock.getElapsedTime() * timescale;
+      updateThetaLength(time);
+      updateRadius(time);
+    })
   return (
     <mesh>
-      <sphereBufferGeometry args={[2, 24, 24]} />
-      <meshStandardMaterial color={'magenta'} />
+      <sphereBufferGeometry args={[radius, 24, 24, 0, Math.PI*2, 0, thetaLength]} />
+      {/* <torusBufferGeometry args={[radius, 0.1, 16, 64]} /> */}
+      <meshStandardMaterial transparent color={'magenta'} opacity={0.7} />
     </mesh>
   )
 }
@@ -15,7 +39,7 @@ function App() {
     <Canvas>
       {/* <ambientLight intesity={0}/> */}
       <pointLight position={[15, 15, 15]} />
-      <Sphere />
+      <ConstrictingHemisphere/>
     </Canvas>
   );
 }
