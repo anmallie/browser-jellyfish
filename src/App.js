22f13347063cd96a802679611d49a20192878032
anmallie <96325117+anmallie@users.noreply.github.com>
2023-04-05 14:25:52 -0700

    render a basic sphere using react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { Canvas } from '@react-three/fiber'
+
+function Sphere() {
+  return (
+    <mesh>
+      <sphereBufferGeometry args={[2, 24, 24]} />
+      <meshStandardMaterial color={'magenta'} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Canvas>
+      {/* <ambientLight intesity={0}/> */}
+      <pointLight position={[15, 15, 15]} />
+      <Sphere />
+    </Canvas>
   );
 }
 
